b007f63b1392719e2f13110a3446488df45eeada
Aaron R Miller <aaron.miller@waweb.io>
2020-08-16 16:14:32 -0700

    Updated Cucumber syntax coloring implementation
    Signed-off-by: Aaron R Miller <aaron.miller@waweb.io>

--- org.agileware.natural.cucumber.ui/src/org/agileware/natural/cucumber/ui/syntaxcoloring/LexicalHighlightingCalculator.java
+++ org.agileware.natural.cucumber.ui/src/org/agileware/natural/cucumber/ui/syntaxcoloring/LexicalHighlightingCalculator.java
@@ -2,38 +2,46 @@ package org.agileware.natural.cucumber.ui.syntaxcoloring;
 
 import java.util.regex.Pattern;
 
+import org.eclipse.xtext.ide.editor.syntaxcoloring.HighlightingStyles;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.AbstractAntlrTokenToAttributeIdMapper;
-import org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultHighlightingConfiguration;
 
-public class LexicalHighlightingCalculator extends
-		AbstractAntlrTokenToAttributeIdMapper {
+public class LexicalHighlightingCalculator extends AbstractAntlrTokenToAttributeIdMapper {
 
 	private static final Pattern QUOTED = Pattern.compile("(?:^'([^']*)'$)|(?:^\"([^\"]*)\")$", Pattern.MULTILINE);
-	
+
 	@Override
 	protected String calculateId(String tokenName, int tokenType) {
-		if(QUOTED.matcher(tokenName).matches()) {
-			return DefaultHighlightingConfiguration.KEYWORD_ID;
+		if("'|'".equals(tokenName)) {
+			return HighlightingConfiguration.TABLE_ID;
 		}
-		if("RULE_STRING".equals(tokenName)) {
-			return DefaultHighlightingConfiguration.STRING_ID;
+		if (QUOTED.matcher(tokenName).matches()) {
+			return HighlightingStyles.KEYWORD_ID;
 		}
-		if("RULE_INT".equals(tokenName)) {
-			return DefaultHighlightingConfiguration.NUMBER_ID;
+		if ("RULE_STRING".equals(tokenName)) {
+			return HighlightingStyles.STRING_ID;
 		}
-		if("RULE_SL_COMMENT".equals(tokenName)) {
-			return DefaultHighlightingConfiguration.COMMENT_ID;
+		if ("RULE_NUMBER".equals(tokenName)) {
+			return HighlightingStyles.NUMBER_ID;
 		}
-		if("RULE_TAGNAME".equals(tokenName)) {
-			return HighlightingConfiguration.TAG;
+		if ("RULE_SL_COMMENT".equals(tokenName)) {
+			return HighlightingStyles.COMMENT_ID;
 		}
-		if("RULE_DOC_STRING".equals(tokenName)) {
-			return HighlightingConfiguration.DOC_STRING;
+		if ("RULE_TAGNAME".equals(tokenName)) {
+			return HighlightingConfiguration.TAG_ID;
 		}
-		if("RULE_TABLE_ROW".equals(tokenName)) {
-			return HighlightingConfiguration.TABLE;
+		if ("RULE_DOC_STRING".equals(tokenName)) {
+			return HighlightingConfiguration.DOC_STRING_ID;
 		}
-		return DefaultHighlightingConfiguration.DEFAULT_ID;
-	}
+		if ("RULE_TABLE_CELL".equals(tokenName)) {
+			return HighlightingConfiguration.TABLE_ID;
+		}
+		if ("RULE_STEP_KEYWORD".equals(tokenName)) {
+			return HighlightingStyles.KEYWORD_ID;
+		}
+		if ("RULE_PLACEHOLDER".equals(tokenName)) {
+			return HighlightingConfiguration.PLACEHOLDER_ID;
+		}
 
+		return HighlightingStyles.DEFAULT_ID;
+	}
 }
